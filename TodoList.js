1963df04bbe89ac8f8614d7fe80e988dee3b97af
Tonhao Santos <ahssantos1@gmail.com>
2023-07-07 17:39:25 -0300

    added TodoList and changed vscode files

---
+++ TodoList.js
@@ -0,0 +1,18 @@
+/*
+  #TODO - Mensagem
+  - [ ] Fazer isso
+  - [x] Feito
+  - [ ] Fazer isso
+
+
+  #FIXME - Mensagem
+  - [ ] Fazer isso
+  - [x] Feito
+  - [ ] Fazer isso
+git 
+
+  #BUG - Mensagem
+  - [ ] Fazer isso
+  - [x] Feito
+  - [ ] Fazer isso
+*/
