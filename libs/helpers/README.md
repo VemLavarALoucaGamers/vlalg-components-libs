a9d01eea57e90199526e0eef530506b391198abe
Tonhao Santos <ahssantos1@gmail.com>
2023-07-21 15:36:05 -0300

    deleted nimbus project, added new vscode snippets and doc helpers

--- libs/helpers/README.md
+++ libs/helpers/README.md
@@ -1,36 +1,43 @@
-[![NPM Version][npm-image]][npm-url]
+# helpers
 
-# O que é?
+## Helpers
 
-Aplicacao basica para teste de publicacao no npm e yarn.
-
-# Instalacao
-```js
-// npm
-npm i test-publish --save
-
-// yarn
-yarn add test-publish
-```
-
-Outras coisas ...
-
-```js
-import { soma2, sub2, fazAlgo } from 'test-publish';
-
-soma2(1, 3);
-sub2(50, 2);
-fazAlgo({
-    tamanho: 'big',
-    cor: 'blue'
-})
-```
-
-# Opcoes
-fazAlgo suporta 2 opcoes, as duas são opcionais:
-
-- **tamanho**: _big / small_ (Default é small)
-- **cor**: _blue / tomato_ (Default é tomato)
-
-[npm-image]: https://img.shields.io/npm/v/@vemlavaraloucagamers/test-publish
-[npm-url]: https://npmjs.org/package/@vemlavaraloucagamers/test-publish
+- isNumber
+- eventListener
+- asyncMapPromise
+- forIn
+- forOf
+- objectIsEmpty
+- objectHasQuantityProperties
+- itIsObject
+- itIsString
+- isEmptyObject
+- objectHasArrayKeys
+- objectHasKey
+- itIsArray
+- populateArray
+- JsonParse
+- jsonStringify
+- interval
+- timeout
+- getMediaQueries
+- isThisMediaQueries
+- isEmpty
+- fixNullToNumber
+- goExternalLocation
+- isValidEmail
+- setLocalStorageVariable
+- removeLocalStorageVariable
+- getLocalStorageVariable
+- isValidPassword
+- trimString
+- removeSpaceInString
+- splitArrayGroup
+- goWhatsapp
+- initials
+- clearStringValues
+- validarCNPJ
+- validarCpf
+- convertHexToRGBA
+- parseStringCurrencyToFloat
+- preventDecimalKeyCodes
--- libs/helpers/index.js
+++ libs/helpers/index.js
@@ -1,55 +1,108 @@
-// Funcao que centraliza o addEventListener
-// wantsUntrusted é opcional
+/**
+ * Function that checks if the parameter is a number
+ * @param {number} value - Param description
+ * @returns {boolean} Return description
+*/
+export const isNumber = (value = '') => {
+  return !!(value && typeof value === 'number' && !isNaN(value))
+}
+
+/**
+ * Function that centralizes the use of addEventListener, wantsUntrusted é opcional
+ * @param {number} event - Type/Event name
+ * @param {function} func - Function to be executed
+ * @param {boolean} wantsUntrusted - Used to specify whether the event can be triggered by untrusted content
+ * @returns It returns nothing by default, only if the defined function returns
+*/
 export const eventListener = async (event, func, wantsUntrusted = false) => {
   document.addEventListener(event, func, wantsUntrusted)
 }
 
-// Funcao async map
+/**
+ * Function created to do Async in `.map` of javascript
+ * @param {array} itens - array of items
+ * @param {function} callback - Function to be executed
+ * @returns Returns a new array of items, as the callback function returns
+ * @example
+ * ```
+ *  const array = ['1', '2', '3', '4']
+ *  const newArray = asyncMapPromise(array, (item, index, array) => item === '2')
+ *  console.log(newArray) // 2
+ * ```
+*/
 export const asyncMapPromise = async (itens, callback) => {
-  // console.time('promise all')
-  
   return await Promise.all(itens.map(async (item, index, array) => {
     // console.log('item -> ', item._id, array[index]._id)
     const newItem = await callback(item, index, array)
 
     return newItem
   }))
-
-  // console.timeEnd('promise all')
 }
 
-// Funcao que verifica se é um obj vazio
+/**
+ * Function that simplifies for...in (same as using for). Has access to the array index, loops over non-numeric indexes. Receives a callback function as the second parameter. The for in loop iterates over a object and each interaction returns a key.
+ * @param {object} value - Object to interact
+ * @param {function} callback - Function to be executed
+ * @returns It returns nothing by default, only if the defined function returns
+*/
+export const forIn = async (value, callback) => {
+  try {
+    for (let i in value) {
+      await callback(value, i)
+    }
+  } catch (error) {
+    console.log('Error in helper forIn')
+  }
+}
+
+/**
+ * Function that simplifies for...of (same as using forEach), does not have access to the array index equal to forEach((v, i) => ...). Does not loop on non-numeric indexes. Takes a callback function as a second parameter.
+ * @param {array|string|map} value - Arrays, Strings, Maps, and others objects who have the method `Symbol.iterator` to interact
+ * @param {function} callback - Function to be executed
+ * @returns It returns nothing by default, only if the defined function returns
+*/
+export const forOf = async (value, callback) => {
+	try {
+    for (let i of value) {
+      await callback(value, i)
+    }
+  } catch (error) {
+    console.error('Error in helper forOf')
+  }
+}
+
+/**
+ * Function that checks if it is an empty object
+ * @param {object} obj - Object to be tested
+ * @returns {boolean} If it's empty or not
+*/
 export const objectIsEmpty = (obj) => {
   if (!itIsObject(obj)) return false
-  return Object.keys(obj).length === 0 && obj.constructor === Object
+
+  return !!(Object.keys(obj).length === 0)
 }
 
-// Funcao que retorna se um objeto possui uma quantidade de propriedades
-export const objectHasQuantityProperties = (obj, count) => {
-  if (Object.keys(obj).length === count) return true
-  return false
-}
+/**
+ * Function that returns if an object has a specific key
+ * @param {object} obj - Object to be tested
+ * @param {string} key - key to search
+ * @returns {boolean} If the objt has the key
+*/
+export const objectHasKey = (obj, key) => {
+  if (!itIsObject(obj)) return false
 
-// Funcao que retorna se é um objeto
-export const itIsObject = obj => {
-  if (obj.constructor === Object) return true
-  return false
-}
+  return !!Object.prototype.hasOwnProperty.call(obj, key)
+} 
 
-// Funcao que retorna se é uma string
-export const itIsString = stg => {
-  if (stg.constructor === String) return true
-  return false
-}
-
-// Funcao que retorna se é um objeto vazio
-export const isEmptyObject = obj => {
-  if (Object.entries(obj).length === 0) return true
-  return false
-}
-
-// Funcao que retorna se um objeto possui keys
+/**
+ * Function that returns whether an object has a list of keys
+ * @param {object} obj - Object to be tested
+ * @param {string[]} keys - List of keys to search
+ * @returns {boolean} if an object has a list of keys
+*/
 export const objectHasArrayKeys = (obj, keys) => {
+  if (!itIsObject(obj)) return false
+
   const objkeys = Object.keys(obj)
 
   if (objkeys.length && keys.length) {
@@ -63,25 +116,62 @@ export const objectHasArrayKeys = (obj, keys) => {
   return false
 }
 
-// Funcao que retorna se um objeto possui uma key x
-export const objectHasKey = (obj, key) => {
-  return !!Object.prototype.hasOwnProperty.call(obj, key)
-}
+/**
+ * Function that returns whether an object has a number of properties
+ * @param {object} obj - Object to be tested
+ * @param {number} amount - amount of properties
+ * @returns {boolean} If you have the amount passed
+*/
+export const objectHasQuantityProperties = (obj, amount) => {
+  if (!itIsObject(obj)) return false
 
-// Funcao que retorna se é um array
-export const itIsArray = item => {
-  if (Array.isArray(item) && item instanceof Array && item.constructor === Array) return true
+  if (Object.keys(obj).length === amount) return true
   return false
 }
 
-// Popular array
-export const populateArray = (destiny = [], origin = []) => {
-  let currentArray = destiny
+/**
+ * Function that returns if it is an object
+ * @param {*} obj - Object to be tested
+ * @returns {boolean} If it is an object
+*/
+export const itIsObject = obj => {
+  if (obj.constructor === Object) return true
+  return false
+}
 
-  if (!destiny.length) {
-    currentArray = origin
+/**
+ * Function that returns if it is an string
+ * @param {*} stg - string to be tested
+ * @returns {boolean} If it is an string
+*/
+export const itIsString = stg => {
+  if (stg.constructor === String) return true
+  return false
+}
+
+/**
+ * Function that returns if it is an array
+ * @param {*} arr - array to be tested
+ * @returns {boolean} If it is an array
+*/
+export const itIsArray = arr => {
+  if (Array.isArray(arr) && arr.constructor === Array) return true
+  return false
+}
+
+/**
+ * Function for populate array
+ * @param {*} outputArray - output array
+ * @param {*} inputArray - input array
+ * @returns {boolean} If it is an array
+*/
+export const populateArray = (outputArray = [], inputArray = []) => {
+  let currentArray = outputArray
+
+  if (!outputArray.length) {
+    currentArray = inputArray
   } else {
-    origin.forEach(item => {
+    inputArray.forEach(item => {
       currentArray.push(item)
     })
   }
@@ -89,55 +179,106 @@ export const populateArray = (destiny = [], origin = []) => {
   return currentArray
 }
 
-// Funcao que retorna uma string JSON em Objeto
-export const JsonParse = item => {
-  return JSON.parse(item)
+/**
+ * Function that returns a JSON string in Object
+ * @param {string} stg - A JSON string
+ * @returns {object} Returns an object
+*/
+export const JsonParse = stg => {
+  return JSON.parse(stg)
 }
 
-// Funcao que retorna um Objeto em string JSON
-export const jsonStringify = item => {
-  return JSON.stringify(item)
+/**
+ * Function that returns an Object in JSON string
+ * @param {object} obg - A object
+ * @returns {string} Returns a JSON string
+*/
+export const jsonStringify = obg => {
+  return JSON.stringify(obg)
 }
 
-// Funcao que centraliza o setInterval
+/**
+ * Function that centralizes the use of setInterval
+ * @param {number} time - Execution time in milleseconds
+ * @param {function} func - Function to be executed
+ * @returns returns nothing
+*/
 export const interval = (time, func) => {
-  setInterval(func, time)
+  const intervalId = setInterval(func, time)
+  clearInterval(intervalId)
 }
 
-// Funcao que centraliza o setTimeout
+/**
+ * Function that centralizes the use of setTimeout
+ * @param {number} time - Execution time in milleseconds
+ * @param {function} func - Function to be executed
+ * @returns returns nothing
+*/
 export const timeout = (time, func) => {
-  setTimeout(func, time)
+  const timeoutId = setTimeout(func, time)
+  clearTimeout(timeoutId)
 }
 
-// Funcao que obtem o media querie em js
+/**
+ * Function that gets the media query in js
+ * @returns {number} media query size
+*/
 export const getMediaQueries = () => {
   return window.innerWidth
 }
 
-// Funcao que verifica se a tela esta em um media querie x
+/**
+ * Function that checks if the screen is in a specific media query
+ * @param {string} newMedia - Media query that will be tested, default value is `(min-width: 400px)`
+ * @returns {boolean} If it is in the informed media query
+*/
 export const isThisMediaQueries = (newMedia = '(min-width: 400px)') => {
   if (window.matchMedia(newMedia).matches) return true
   return false
 }
 
-// Funcao que verifica se é vazio e retorna booleano
+/**
+ * Function that checks if a variable is empty
+ * @param {number} value - variable to be checked
+ * @returns {boolean} If a variable is empty
+*/
 export const isEmpty = value => {
   if (value) return false
 
   return true
 }
 
-// Funcao que transforma null em 0
+/**
+ * Function that turns null into 0
+ * @param {null} value - variable to be changed
+ * @returns {number} Changed value
+*/
 export const fixNullToNumber = value => {
   if (value === null) return 0
   return value
 }
 
-// Funcao que redireciona para uma URL externa (_self, _parent, _top, _blank)
+/**
+ * Function that redirects to an external URL
+ * @param {string} url - Url to go
+ * @param {string} type - variable to be changed, accepted values `_self`, `_parent`, `_top` and `_blank`.
+ * @returns returns nothing
+*/
 export const goExternalLocation = (url = '', type = '_blank') => {
   if (!url) return
 
   window.open(url, type)
+}
+
+/**
+ * Function that validates email
+ * @param {string} email - Email that must be checked
+ * @returns {boolean} If it is valid
+*/
+export const isValidEmail = email => {
+  const emailPattern = /^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{1,3})+$/
+
+  return emailPattern.test(email)
 }
 
 // Funcao seta variavel no localstorage
@@ -155,13 +296,6 @@ export const getLocalStorageVariable = (name = '') => {
   if (name) return localStorage.removeItem(name)
 }
 
-// Funcao que valida email
-export const isValidEmail = email => {
-  const emailPattern = /^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{1,3})+$/
-
-  return emailPattern.test(email)
-}
-
 // Funcao que valida senha
 export const isValidPassword = password => {
   if (password && password.length >= 8) return true
@@ -177,21 +311,6 @@ export const trimString = value => {
 // Funcao remove espacos
 export const removeSpaceInString = value => {
   return value.replace(/\s+/g, '')
-}
-
-// Funcao seta a role local
-export const setLocalRole = role => {
-  localStorage.setItem('system_role', role)
-}
-
-// Funcao remove a role local
-export const removeLocalRole = () => {
-  localStorage.removeItem('system_role')
-}
-
-// Funcao retorna a role local
-export const getLocalRole = () => {
-  return localStorage.getItem('system_role')
 }
 
 // Funcao de dividir array em grupos
@@ -231,24 +350,22 @@ export const goWhatsapp = (phone = '', text = '', type = 'wa') => {
   return message
 }
 
-// Função que pega o TimezoneOffset do usuário
-export const getTimezoneOffset = () => {
-  return new Date().getTimezoneOffset()
-}
-
 // Função que retorna as letras iniciais da string
 export const initials = (string) => {
+  $log.method('File: src/helpers/functions.js --initials--')
   const words = string.split(/[\s-]+/)
   return words.map(word => word.substr(0, 1)).join('').substr(0, 2).toUpperCase()
 }
 
 // Função que elimina qualquer caracter especial da string
 export const clearStringValues = (string) => {
+  $log.method('File: src/helpers/functions.js --clearStringValues--')
   if (string) return string.replace(/[^\w\s]/gi, '')
 }
 
 // Funcao que valida cnpj
 export const validarCNPJ = (value) => {
+  $log.method('File: src/helpers/functions.js --validarCNPJ--')
   const localValue = clearStringValues(value)
 
   if (!localValue) return false
@@ -318,66 +435,42 @@ export const validarCNPJ = (value) => {
 
 // Funcao que valida cpf
 export const validarCpf = (value) => {
+  $log.method('File: src/helpers/functions.js --validarCpf--')
   const cpf = clearStringValues(value)
 
   let soma = 0
   let resto
 
-  if (cpf === '00000000000') return false
+  if (cpf == "00000000000") return false
 
-  if (cpf.length !== 11 ||
-    cpf === '00000000000' ||
-    cpf === '11111111111' ||
-    cpf === '22222222222' ||
-    cpf === '33333333333' ||
-    cpf === '44444444444' ||
-    cpf === '55555555555' ||
-    cpf === '66666666666' ||
-    cpf === '77777777777' ||
-    cpf === '88888888888' ||
-    cpf === '99999999999') return false
+  if (cpf.length != 11 ||
+    cpf == "00000000000" ||
+    cpf == "11111111111" ||
+    cpf == "22222222222" ||
+    cpf == "33333333333" ||
+    cpf == "44444444444" ||
+    cpf == "55555555555" ||
+    cpf == "66666666666" ||
+    cpf == "77777777777" ||
+    cpf == "88888888888" ||
+    cpf == "99999999999")
+      return false
 
   for (let i=1; i<=9; i++) soma = soma + parseInt(cpf.substring(i-1, i)) * (11 - i)
   resto = (soma * 10) % 11
 
-  if ((resto === 10) || (resto === 11)) resto = 0
-  if (resto !== parseInt(cpf.substring(9, 10)) ) return false
+  if ((resto == 10) || (resto == 11))  resto = 0
+  if (resto != parseInt(cpf.substring(9, 10)) ) return false
 
   soma = 0
   for (let i = 1; i <= 10; i++) soma = soma + parseInt(cpf.substring(i-1, i)) * (12 - i)
 
   resto = (soma * 10) % 11
 
-  if ((resto === 10) || (resto === 11))  resto = 0
-  if (resto !== parseInt(cpf.substring(10, 11) ) ) return false
+  if ((resto == 10) || (resto == 11))  resto = 0
+  if (resto != parseInt(cpf.substring(10, 11) ) ) return false
 
   return true
-}
-
-// Funcao que simplifica o for...in (mesmo que usar for), tem acesso ao index do array
-// Realiza loop em index nao numericos
-// Recebe uma funcao de callback como segundo parametro
-export const forIn = async (value, callback) => {
-  try {
-    for (const i in value) {
-      await callback(value, i)
-    }
-  } catch (error) {
-    console.log('Error in helper forIn')
-  }
-}
-
-// Funcao que simplifica o for...of (mesmo que usar forEach), nao tem acesso ao index do array igual ao forEach((v, i) => ...)
-// Nao realiza loop em index nao numericos
-// Recebe uma funcao de callback como segundo parametro
-export const forOf = async (value, callback) => {
-  try {
-    for (const i of value) {
-      await callback(value, i)
-    }
-  } catch (error) {
-    console.error('Error in helper forOf')
-  }
 }
 
 // Função que converte Hex em RGBA
@@ -396,7 +489,9 @@ export const convertHexToRGBA = (hexCode, opacity) => {
 }
 
 export const parseStringCurrencyToFloat = async (string) => {
-  const currentValue = string.substring(2, string.length).replace('.', '').replace(',', '.')
+  const currentValue = string.substring(2, string.length)
+  .replace('.', '')
+  .replace(',', '.')
 
   return parseFloat(currentValue)
 }
@@ -404,9 +499,4 @@ export const parseStringCurrencyToFloat = async (string) => {
 //Função valida se há ponto, traço ou 'e' no input number decimal
 export const preventDecimalKeyCodes = (keyCode) => {
   return !!(keyCode === 46 || keyCode === 45 || keyCode === 101)
-}
-
-//Função valida se um numero
-export const isNumber = (value = '') => {
-  return !!(value && typeof value === 'number' && !isNaN(value))
 }
